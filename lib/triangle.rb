02acfae4b3ace4d4776129fafd0bedb2933ab7ea
Javier Aguilar <coyote176@msn.com>
2020-08-31 00:31:19 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,7 @@ class Triangle
     @c=c
   end
   def kind
-    if !(@a > 0 && @b >0 && @c >0 && @a+@b >@c && @a+@c >@b && @b+@c > @a)
+if !(@a > 0 && @b >0 && @c >0 && @a+@b >@c && @a+@c >@b && @b+@c > @a)
       raise TriangleError
     elsif @a == @b && @b == @c
       :equilateral
